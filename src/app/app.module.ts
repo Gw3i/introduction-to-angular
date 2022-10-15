4c14be19e0b576abf3784bd1a2e73455e84f4e9f
Vlady <contact.johngoldco@gmail.com>
2022-10-15 11:38:48 +0300

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HousingListComponent } from './housing-list/housing-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HousingListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
